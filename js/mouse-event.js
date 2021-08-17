3c01b5003156bf9ddb0fbc25173edc96b34d5d69
Mathieu <mathieu.linder.dev@gmail.com>
2021-08-17 03:36:19 +0200

    JS mouse event

--- js/mouse-event.js
+++ js/mouse-event.js
@@ -1,9 +1,66 @@
 const mouseEvent = document.querySelector('.mouseEvent');
 const horizontal = document.querySelector('.horizontal');
 const vertical = document.querySelector('.vertical');
+const mouseMove = document.getElementById('mouseMove');
+const container = document.querySelector('.mouseEventContainer');
+const coord = document.querySelector('.coordinates');
+// const tglBtn = document.getElementById('toggle-btn');
+// const navBar = document.getElementById('navbar');
+
+
+
+
+// ************* Affichage dynamique des coordonnées de la souris **********************
 
 mouseEvent.addEventListener('mousemove', (e) => {
-    console.log(e);
-    horizontal.innerHTML = e.x;
-    vertical.innerHTML = e.y
+    // console.log(e);
+    horizontal.innerHTML = e.pageX;
+    vertical.innerHTML = e.pageY;
+});
+
+
+
+
+// ************** Modification du style du cercle de la souris *************************
+
+function enter () {
+    mouseMove.style.border = "3px solid var(--color5)";
+}
+
+mouseEvent.addEventListener('mouseenter', enter);
+container.addEventListener('mouseenter', enter);
+horizontal.addEventListener('mouseenter', enter);
+vertical.addEventListener('mouseenter', enter);
+coord.addEventListener('mouseenter', enter);
+
+
+mouseEvent.addEventListener('mouseout', () => {
+    mouseMove.style.border = "3px solid var(--color3)";
+});
+
+window.addEventListener('mousemove', (e) => {
+    mouseMove.style.left = e.pageX + "px";
+    mouseMove.style.top = e.pageY + "px";
+    mouseMove.style.visibility = "visible";
+    mouseMove.style.transform = "translate(-50%, -50%)";
+});
+
+
+
+// ************* Cache le cercle de la souris sur hover de la navbar et son bouton ***************
+
+tglBtn.addEventListener('mouseover', () => {
+    mouseMove.style.opacity = '0';
+});
+
+tglBtn.addEventListener('mouseout', () => {
+    mouseMove.style.opacity = '1';
+})
+
+navBar.addEventListener('mouseover', () => {
+    mouseMove.style.opacity = '0';
+});
+
+navBar.addEventListener('mouseout', () => {
+    mouseMove.style.opacity = '1';
 })
