9c85efc1553c117647d17464c8cdd981000374d8
Mathieu <mathieu.linder.dev@gmail.com>
2021-08-24 19:23:26 +0200

    Scroll Event et Form Event

--- js/some-events.js
+++ js/some-events.js
@@ -1,3 +1,5 @@
+/************************* Keypress Event ************************ */
+
 const keypressContainer = document.querySelector('.keypress');
 const key = document.getElementById('key');
 const bravo = document.querySelector('.bravo');
@@ -6,7 +8,7 @@ const ring = (key) => {
     const audio = new Audio();
     audio.src = './assets/ring/' + key + '.mp3';
     audio.play();
-}
+};
 
 document.addEventListener('keypress', (e) => {
     key.textContent = e.key;
@@ -15,6 +17,49 @@ document.addEventListener('keypress', (e) => {
     } else {
         bravo.classList.remove('active');
     }
-    ring(e.key);
+
+    if (e.key === "i") {
+        ring(e.key);
+    }
 });
 
+
+/************************* Scroll Event ************************ */
+
+const scrollBar = document.getElementById('scrollBar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 150) {
+        scrollBar.style.top = 0; 
+    } else {
+        scrollBar.style.top = "-80px";
+    }
+});
+
+/************************* Form Event ************************ */
+
+const name = document.getElementById('name');
+const select = document.getElementById('select');
+const form = document.querySelector('.form');
+
+let show = document.querySelector('.showForm');
+let pseudo = "";
+let language ="";
+
+name.addEventListener('input', (e) => {
+    pseudo = e.target.value;
+});
+
+select.addEventListener('input', (e) => {
+    language = e.target.value;
+});
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (cg.checked) {
+        show.innerHTML = `Le langage préféré de ${pseudo} est ${language} !`;
+    } else {
+        alert('Veuillez accepter les conditions générales');
+    }
+});
